8badc5e176ac8967e08ede7317d85982e6a85b0c
CharlesAragao <charless.dev17@gmail.com>
2025-06-02 16:24:05 -0300

    Controllers/register e authentication

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,7 @@ import supabase from '../config/supabase';
 import  bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+// Cadastro de usuário
 const registerUser = async (request, response) => {
     // Desestruturação do objeto
     const { nome, email, senha } = request.body;
@@ -11,18 +12,60 @@ const registerUser = async (request, response) => {
 
     // Abre a conexão com o Supabase
     const { data, error } = await supabase
-    .from('user')
-    .insert([
-        {
-            nome,
-            email,
-            senha: passwordHashed
-        }
+        .from('users')
+        .insert([
+            {
+                nome,
+                email,
+                senha: passwordHashed
+            }
     ]);
 
-    if (erro) {
-        return resposta.status(500).json({ error });
+    if (error) {
+        return resposta.status(500).json({ 
+            error: "Erro: ", error });
     }
 
+    response.status(201).json({
+        mensagem: "usuário cadastrado com sucesso!"
+    });
 };
 
+// Login
+const authentication = async (request, response) => {
+    // Desestruturação do objeto
+    const { email, sanha } = request.body;
+
+    // Abra a conexão com o Supabase
+    const { data: user, error } = await supabase
+        .from('users')
+        .select("*")
+        .eq('email', email)
+        .single();
+
+    if (error || !users) {
+        return response.status(401).json({
+            mensagem: "Credenciais inválidas"
+        });
+    }
+
+    // Criptografa a senha e compara com a senha do DB
+    const senhaEnviada = await bcrypt.compare(senha, user.senha);
+
+    if (!senhaEnviada) {
+        return response.status(401).json({
+            mensagem: "Credenciais inválidas"
+        });
+    }
+
+    // Gera o Token
+    const token = jwt.sign(
+       { id: user.id },
+       process.env.JWT_SECRET,
+       { expiresIn: "1d" },
+    );
+
+    // Devolve o Token gerado
+    response.json({ token });
+
+};
